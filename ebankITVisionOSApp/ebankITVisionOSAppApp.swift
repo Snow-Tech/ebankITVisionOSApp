2af8f0b5376cdfbf5e4e5b96d99d32a96276bf5d
Brian Vieira <brian.vieira@ebankit.com>
2023-10-02 13:58:04 +0100

    Initial Commit

---
+++ ebankITVisionOSApp/ebankITVisionOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebankITVisionOSAppApp.swift
+//  ebankITVisionOSApp
+//
+//  Created by ebankit on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ebankITVisionOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
