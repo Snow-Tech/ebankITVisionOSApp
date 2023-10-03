867da0c045b484b1571fd8e17bc546fdf2bbee3b
Brian Vieira <brian.vieira@ebankit.com>
2023-10-03 16:33:39 +0100

    basic visionOS

---
+++ ebankITVisionOSApp/MultiWindowView.swift
@@ -0,0 +1,18 @@
+//
+//  MultiWindowView.swift
+//  ebankITVisionOSApp
+//
+//  Created by ebankit on 02/10/2023.
+//
+
+import SwiftUI
+
+struct MultiWindowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiWindowView()
+}
